febbb07ba8f0bae623e447b49d3e0b8e925a4094
Alcoto95 <alberto.copado@wizeline.com>
2019-08-15 12:08:00 -0500

    feat: fix elements and add allure reporter

--- page_objects/homePage.js
+++ page_objects/homePage.js
@@ -4,8 +4,8 @@ class HomePage {
   constructor() {
     this.menuItems = Selector('div#top_icons')
     this.newTaskBtn = Selector('.agenda_add_task a')
-    this.newTaskText = Selector('div.richtext_editor')
-    this.submitTask = Selector('a.submit_btn')
+    this.newTaskText = Selector('div.DraftEditor-editorContainer > div')
+    this.submitTask = Selector('button.item_editor_submit')
     this.taskItem = Selector('li.task_item')
     this.taskName = Selector('span.text')
   }
